c70c5d3c40456fd8a259b37c29e05dffe91a12a7
James Young <jamesanthyoung@outlook.com>
2023-09-10 08:31:52 +1000

    Section 2 Updates

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,24 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let first = "Jonas";
+let firstName = "Matilda";
+let firstNamePerson = "James";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $new = 27;
+let person = "Jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
